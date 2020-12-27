dd2750f7ef13fd0f303b81f0bb4cceae958bb758
Neeraj Singh <singh4neeraj@gmail.com>
2020-12-28 02:58:50 +0530

    my portfolio v02

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk'
 
 import Reducers from './reducers'
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
 const middlewares = [reduxThunk];
-const Store  = createStore(Reducers, composeEnhancers(
-	applyMiddleware(...middlewares)
-));
+const Store  = createStore(Reducers, applyMiddleware(...middlewares));
 
 export default Store;
 
